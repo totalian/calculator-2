bb465ef84d9dee9ea714d1aa30de2b2b65432018
Zeki Mirza <zeki.mirza@googlemail.com>
2021-09-27 22:30:16 +0100

    added number inputs and operation inputs

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
-//Dom elements
-const btnSpace = document.querySelector('.buttons')
+//Dom Elements
+const display = document.querySelector('.display')
+const clearBtn = document.querySelector('#clear-btn')
+const operatorBtns = document.querySelectorAll('.operator-btn')
+const numberBtns = document.querySelectorAll('.number-btn')
+const equalsBtn = document.querySelector('.equals-btn')
 
-// make and populate calculator with buttons
-const makeButtons = () => {
-    for (let index = 0; index < 19; index++) {
-        let btn = document.createElement('div')
-        btn.dataset.index = index
-        btnSpace.appendChild(btn)
-    }
+//Calculator
+const calculator = {
+    display: "",
+    memory: "",
+    operationMode: undefined,
+    add: (a,b) => a + b,
+    multiply: (a,b) => a * b,
+    divide: (a,b) => a / b,
+    subtract: (a,b) => a - b,
 }
+
+//render calcualtor display to DOM display function
+const render = () => display.textContent = calculator.display
+
+//Press a number
+Array.from(numberBtns).forEach(element => {
+    element.addEventListener('click', e => {
+        if(display.textContent.length < 9){
+            calculator.display = calculator.display  + e.target.textContent
+            render()
+        }
+    })
+});
+
+//press an operations
+Array.from(operatorBtns).forEach(element => {
+    element.addEventListener('click', e => {
+        switch(element.textContent){
+            case 'x':
+                calculator.operationMode = calculator.multiply
+                break
+            case '/':
+                calculator.operationMode = calculator.divide
+                break
+            case '-':
+                calculator.operationMode = calculator.subtract
+                break
+            case '+':
+                calculator.operationMode = calculator.add
+                break        
+        }
+        if(calculator.memory == ''){
+            calculator.memory = calculator.display
+        }
+    })
+})
